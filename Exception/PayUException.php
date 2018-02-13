5af32d2a398076e7ffaa1fe3a5688f7b50ca610b
Krzysiek Lobermajer <klobermajer@accesto.pl>
2018-02-13 18:40:08 +0100

    added model to payu exception

--- Exception/PayUException.php
+++ Exception/PayUException.php
@@ -12,26 +12,39 @@ use Psr\Http\Message\ResponseInterface;
  */
 class PayUException extends HttpException
 {
-    public static function newInstance($status)
+    private $model;
+
+    public static function newInstance($status = null, $model = null)
     {
         $label = 'PayUException';
 
         $parts = array(
             $label
         );
-        if (property_exists($status, 'statusLiteral')) {
-            $parts[] = '[reason literal] ' . $status->statusLiteral;
-        }
-        if (property_exists($status, 'statusCode')) {
-            $parts[] = '[status code] ' . $status->statusCode;
-        }
-        if (property_exists($status, 'statusDesc')) {
-            $parts[] = '[reason phrase] ' . $status->statusDesc;
+        if ($status) {
+            if (property_exists($status, 'statusLiteral')) {
+                $parts[] = '[reason literal] ' . $status->statusLiteral;
+            }
+            if (property_exists($status, 'statusCode')) {
+                $parts[] = '[status code] ' . $status->statusCode;
+            }
+            if (property_exists($status, 'statusDesc')) {
+                $parts[] = '[reason phrase] ' . $status->statusDesc;
+            }
         }
         $message = implode(PHP_EOL, $parts);
 
         $e = new static($message);
+        $e->model = $model;
 
         return $e;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModel()
+    {
+        return $this->model;
     }
 }
